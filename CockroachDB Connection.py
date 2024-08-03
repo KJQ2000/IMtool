85084ad4a148d6387e3f7fb6f7c0530054abc468
Jun Qiang <keongchai1988@gmail.com>
2024-08-03 16:49:55 +0800

    Cockroach Database Connected

--- CockroachDB Connection.py
+++ CockroachDB Connection.py
@@ -1,44 +1,55 @@
 import os
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+import psycopg2
 
-engine = create_engine('postgresql://junqiang:e9W8uEXxKAljcwc4z3h5Xg@konghin-6756.6xw.aws-ap-southeast-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full')
+conn = psycopg2.connect(os.environ["DATABASE_URL"])
 
-conn = engine.connect()
+with conn.cursor() as cur:
+    cur.execute("SELECT now()")
+    res = cur.fetchall()
+    conn.commit()
+    print(res)
 
-metadata = MetaData()
+# import os
+# from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 
-# Reflect the existing table schema from the database
-metadata.reflect(bind=engine)
+# engine = create_engine('postgresql://junqiang:e9W8uEXxKAljcwc4z3h5Xg@konghin-6756.6xw.aws-ap-southeast-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full')
 
-# Access the existing table
-users_table = metadata.tables['user']
+# conn = engine.connect()
 
-# # Define a table
-# users_table = Table(
-#     'users',  # Table name
-#     metadata,
-#     Column('id', Integer, primary_key=True),
-#     Column('name', String),
-#     Column('age', Integer)
-# )
+# metadata = MetaData()
 
-# Define the existing table
-users_table = Table('user', metadata, autoload=True, autoload_with=engine)
+# # Reflect the existing table schema from the database
+# metadata.reflect(bind=engine)
 
-# # Insert data into the table
-# insert_stmt = users_table.insert().values(name='Alice', age=25)
-# conn.execute(insert_stmt)
+# # Access the existing table
+# users_table = metadata.tables['user']
 
-# Select data from the table
-select_stmt = users_table.select()
-result = conn.execute(select_stmt).fetchall()
-print("All users:")
-for row in result:
-    print(row)
+# # # Define a table
+# # users_table = Table(
+# #     'users',  # Table name
+# #     metadata,
+# #     Column('id', Integer, primary_key=True),
+# #     Column('name', String),
+# #     Column('age', Integer)
+# # )
 
-# # Update data in the table
-# update_stmt = users_table.update().where(users_table.c.name == 'Alice').values(age=40,id=1)
-# conn.execute(update_stmt)
+# # Define the existing table
+# users_table = Table('user', metadata, autoload=True, autoload_with=engine)
 
-# Close the connection
-conn.close()
+# # # Insert data into the table
+# # insert_stmt = users_table.insert().values(name='Alice', age=25)
+# # conn.execute(insert_stmt)
+
+# # Select data from the table
+# select_stmt = users_table.select()
+# result = conn.execute(select_stmt).fetchall()
+# print("All users:")
+# for row in result:
+#     print(row)
+
+# # # Update data in the table
+# # update_stmt = users_table.update().where(users_table.c.name == 'Alice').values(age=40,id=1)
+# # conn.execute(update_stmt)
+
+# # Close the connection
+# conn.close()
